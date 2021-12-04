abdcbdd067a162737a902c8145745f990acaa56b
wen Tim <wentim@wende-MacBook-Pro.local>
2021-12-04 16:43:51 +0800

    feat: threeJS 學習，改用 express serve 檔案

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+
+app.use('/libs', express.static(path.join(__dirname, 'libs')));
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/src', express.static(path.join(__dirname, 'src')));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(3000, () => {
+  console.log('app listening at http://localhost:3000')
+});
